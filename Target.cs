676963ae8d8b1d5ba7903c56427a8e0feb333bd3
FiletDrengen <sami.amir.assad@gmail.com>
2020-11-09 00:15:07 +0100

    its working

--- Target.cs
+++ Target.cs
@@ -3,6 +3,8 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace DuckHunt
 {
